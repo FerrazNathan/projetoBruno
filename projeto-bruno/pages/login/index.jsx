d725df6b1d046bcad0e553275eba66850a932c18
Nathan Ferraz <nathanferraz@hotmail.com>
2022-03-30 15:50:51 -0300

    add eslint and husky

---
+++ projeto-bruno/pages/login/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Head from 'next/head'
+import Form from '../../components/Login'
+import * as S from './styleLogin'
+import { dataText } from '../../src/common/constants/dataText'
+
+
+export default function Login() {
+	return (
+		<S.Container>
+			<Head>
+				<title>{dataText.TITLEPAGE}</title>
+			</Head>
+			<S.Content>
+				<S.Box>
+					<S.Title>{dataText.TITLE}</S.Title>
+					<p>{dataText.DATE}</p>
+				</S.Box>
+				<Form />
+			</S.Content>
+		</S.Container>
+	)
+}
---
+++ projeto-bruno/src/common/constants/dataText.js
@@ -0,0 +1,9 @@
+export const dataText = {
+	TITLEPAGE: 'Sandy & Bruno',
+	TITLE: 'Bruno Macedo & Sandy Bruna',
+	DATE: '15/11/2022',
+	LOGIN: 'login',
+	TITLELOGIN: 'Esse site exige senha para acessá-lo:',
+	TITLEBUTTON: 'Acessar',
+
+}
