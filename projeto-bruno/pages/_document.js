1f869a2c3fcce197ce3abeff7edbb6aeb18d8fc2
Felipe Paulino <felipepaulino@hotmail.com>
2022-04-08 09:19:03 -0300

    fix codigos

--- projeto-bruno/pages/_document.js
+++ projeto-bruno/pages/_document.js
@@ -1,39 +1,39 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Head, Main, NextScript } from 'next/document'
 // Import styled components ServerStyleSheet
-import { ServerStyleSheet } from 'styled-components';
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    // Step 1: Create an instance of ServerStyleSheet
-    const sheet = new ServerStyleSheet();
+	static getInitialProps({ renderPage }) {
+		// Step 1: Create an instance of ServerStyleSheet
+		const sheet = new ServerStyleSheet()
 
-    // Step 2: Retrieve styles from components in the page
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />),
-    );
+		// Step 2: Retrieve styles from components in the page
+		const page = renderPage((App) => (props) =>
+			sheet.collectStyles(<App {...props} />),
+		)
 
-    // Step 3: Extract the styles as <style> tags
-    const styleTags = sheet.getStyleElement();
+		// Step 3: Extract the styles as <style> tags
+		const styleTags = sheet.getStyleElement()
 
-    // Step 4: Pass styleTags as a prop
-    return { ...page, styleTags };
-  }
+		// Step 4: Pass styleTags as a prop
+		return { ...page, styleTags }
+	}
 
-  render() {
-    return (
-      <html>
-        <Head>
-          {/* Step 5: Output the styles in the head  */}
-          {this.props.styleTags}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link href="https: //fonts.googleapis.com/css2? family= Dancing+Script:wght@600 & display=swap" rel="stylesheet" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
-  }
+	render() {
+		return (
+			<html>
+				<Head>
+					{/* Step 5: Output the styles in the head  */}
+					{this.props.styleTags}
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+					<link href="https: //fonts.googleapis.com/css2? family= Dancing+Script:wght@600 & display=swap" rel="stylesheet" />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</html>
+		)
+	}
 }
